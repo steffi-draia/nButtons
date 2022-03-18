ad31919d297c23223fa9a0dfba84106f6b5c24f1
steffi-draia <stefania.draia@yahoo.com>
2022-03-18 14:11:43 +0100

    update

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-var isWinner = -1;
-
-function generateNButtons() {
-  var div1 = document.getElementById('div1');
-	var nButtons = document.getElementById('user-input').value;
-  console.log(nButtons.value);
-  for (var i = 0; i < nButtons; ++i) {
-  	var createButton = document.createElement('button');
-    createButton.setAttribute("id", i);
-    createButton.addEventListener("click", isWinningButton);
-    createButton.innerHTML = "Click me!";
-    div1.appendChild(createButton);
-  }
-  isWinner = chooseRandomButtonId(nButtons);
-}
+var winnerButtonId = -1;
 
 function chooseRandomButtonId(max) {
   return Math.floor(Math.random() * max);
 }
 
+function generateButtons() {
+  let div1 = document.getElementById('div1');
+  let noOfButtons = document.getElementById('user-input').value;
+  for (let i = 0; i < noOfButtons; ++i) {
+    let buttonElement = document.createElement('button');
+    buttonElement.setAttribute("id", i);
+    buttonElement.addEventListener("click", isWinningButton);
+    buttonElement.innerHTML = "Click me!";
+    div1.appendChild(buttonElement);
+  }
+  winnerButtonId = chooseRandomButtonId(noOfButtons);
+}
+
 function isWinningButton(e) {
-	var buttonId = e.target.id;
-  var clickedButton = document.getElementById('showMessage');
-  if (buttonId == isWinner) {
-    clickedButton.innerHTML = "Well done! Winning Button chosen!";
+  let clickedButtonId = e.target.id;
+  let showResult = document.getElementById('showMessage');
+  if (clickedButtonId == winnerButtonId) {
+    showResult.innerHTML = "Well done! Winning Button chosen!";
   } else {
-    clickedButton.innerHTML = "Maybe you will have better luck next time";
+    showResult.innerHTML = "Maybe you will have better luck next time";
   }
 }
